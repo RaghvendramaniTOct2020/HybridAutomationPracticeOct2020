8711244a60d8d3b6afd1cc730349cda7e96f7eb0
Raghvendrama Tiwari <raghvendramani.tiwari@gmail.com>
2021-02-18 00:11:38 -0500

    Latest_changes_18_feb

--- src/pages/AuthenticationPage.java
+++ src/pages/AuthenticationPage.java
@@ -6,26 +6,32 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import base.PreDefinedAction;
+import util.PropertiesFileReader;
 
 public class AuthenticationPage extends PreDefinedAction{
+	PropertiesFileReader authPage;
+	
+	public AuthenticationPage() {
+		authPage=new PropertiesFileReader("./src/pageProperties/authenticationPageProperties.properties");
+	}
 	
 	public void enterEmailAdress(String email) {
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email_create")))
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(authPage.getValue("createAccountEmail"))))
 		.sendKeys(email);
 		System.out.println("Enter email address");
 	}
 	
 	public CreateAccountPage clickOnCreateAccount() {
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("SubmitCreate"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.id(authPage.getValue("CreateAccountPage")))).click();
 		System.out.println("Clicked on the submit button to fill the other details");
 		return new CreateAccountPage();
 	}
 	
 	public boolean isAuthenticationHeaderVisible() {
 		WebDriverWait wait = new WebDriverWait(driver, 5);
-		WebElement element=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='	Authentication']")));
+		WebElement element=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(authPage.getValue("authenticationHeaderVisible"))));
 		return element.isDisplayed();
 	}
 	
@@ -38,26 +44,26 @@ public class AuthenticationPage extends PreDefinedAction{
 
 	public void clickOnSignInButton() {
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#SubmitLogin"))).click();
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(authPage.getValue("SignInButton")))).click();
 		System.out.println("STEP - Click on sign button");
 	}
 
 	public void enterPasswordInLogin(String pwd) {
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='passwd']"))).sendKeys(pwd);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(authPage.getValue("passwordInLogin")))).sendKeys(pwd);
 		System.out.println("STEP - Enter Password on login section");
 	}
 
 	public void enterEmailAddressInLogin(String emailAddress) {
 		WebDriverWait wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#email"))).sendKeys(emailAddress);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(authPage.getValue("emailAddressInLogin")))).sendKeys(emailAddress);
 		System.out.println("STEP - Enter email address on login section");
 	}
 
 	public String verifyErrorMessage() {
 		WebDriverWait wait = new WebDriverWait(driver, 30);
 		return wait.until(
-				ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[class='alert alert-danger'] li")))
+				ExpectedConditions.visibilityOfElementLocated(By.cssSelector(authPage.getValue("loginErrorMessage"))))
 				.getText();
 	}
 }
